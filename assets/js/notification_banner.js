fe26dd6334f53f3aca2ffba9a4d7fbeb1abf8920
Florent Huck <flovntp@gmail.com>
2024-04-17 08:42:43 +0200

    guidance for user

--- assets/js/notification_banner.js
+++ assets/js/notification_banner.js
@@ -1,7 +1,7 @@
 import $ from 'jquery';
 
 // please customize these values with your own project info
-var projectId = 'lwt7md5tlj3lo';
+var projectId = 'abcdefg12345';
 var environment = 'main';
 var interval = 2000; // 2sec
 
@@ -19,9 +19,11 @@ function checkProjectStatus(projectId) {
         type: 'GET',
         contentType: 'application/json'
     }).done(function(response) {
+        // application is not available
         if(!response.available) {
+            // Do whatever you want to make your visitor wait for the application to be back
             Notiflix.Loading.custom(
-                'A deployment is in progress, please wait for the application to be live. This should take less than a minute.',
+                'A new deployment of your application has started, please wait for the application to be back. This should take less than a minute.',
                 {
                     customSvgUrl: 'https://www.negacarbone.earth/wp-content/uploads/2021/12/anim_shadok_01.gif',
                     svgSize: '400px',
@@ -31,11 +33,13 @@ function checkProjectStatus(projectId) {
 
                 });
         } else {
+            // application is back
             Notiflix.Loading.remove();
         }
     });
 }
 
+// check project status every interval (default to 2sec).
 setInterval(function() {
     checkProjectStatus(projectId);
 }, interval);
