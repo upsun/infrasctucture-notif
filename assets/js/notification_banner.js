72a6c82376d88a9184e13f3ddccee81aab69e1ce
Florent Huck <flovntp@gmail.com>
2024-04-19 10:00:48 +0200

    removing Jquery

--- assets/js/notification_banner.js
+++ assets/js/notification_banner.js
@@ -1,45 +1,50 @@
-import $ from 'jquery';
-
-// please customize these values with your own project info
-var projectId = 'abcdefg12345';
-var environment = 'main';
-var interval = 2000; // 2sec
+// please customize
+const projectId = 'abcdef12345';
+const environment = 'main';
+const interval = 2000; // 2sec
+const endpoint = 'https://main-bvxea6i-ghijkl6789.eu-1.platformsh.site' + '/api/';
 
 /**
- * Function to check project status and display the Notiflix banner if a deployment is in progress.
+ * Function that check project status and display a Notiflix banner if a deployment is in progress.
  * Notiflix Documentation: https://notiflix.github.io/documentation
  *
- * @param projectId
  * @returns void
  */
-function checkProjectStatus(projectId) {
-    return $.ajax({
-        url: 'https://status.woop.ly/api/' + projectId + '/' + environment,
+async function checkProjectStatus() {
+    const response = await fetch(endpoint + projectId + '/' + environment, {
+        method: 'GET',
         crossDomain: true,
-        type: 'GET',
-        contentType: 'application/json'
-    }).done(function(response) {
-        // application is not available
-        if(!response.available) {
-            // Do whatever you want to make your visitor wait for the application to be back
-            Notiflix.Loading.custom(
-                'A new deployment of your application has started, please wait for the application to be back. This should take less than a minute.',
-                {
-                    customSvgUrl: 'https://raw.githubusercontent.com/upsun/infrasctucture-notif/main/assets/images/anim_shadok_01.gif',
-                    svgSize: '400px',
-                    backgroundColor: 'rgba(0,0,0,0.8)',
-                    messageMaxLength: 300,
-                    messageFontSize: '20px',
-
-                });
-        } else {
-            // application is back
-            Notiflix.Loading.remove();
+        contentType: 'application/json',
+        headers: {
+            'Content-Type': 'application/json'
         }
     });
+
+    if (!response.ok) {
+        // do something
+    }
+
+    const body = await response.json();
+
+    if (!body.available) {
+        // Do whatever you want to make your visitor wait for the application to be back
+        Notiflix.Loading.custom(
+            'A new deployment of the application has been triggered, please wait for the app to be back. This should take a few.',
+            {
+                customSvgUrl: 'https://raw.githubusercontent.com/upsun/infrasctucture-notif/main/assets/images/anim_shadok_01.gif',
+                svgSize: '400px',
+                backgroundColor: 'rgba(0,0,0,0.8)',
+                messageMaxLength: 300,
+                messageFontSize: '20px',
+
+            });
+    } else {
+        // application is back
+        Notiflix.Loading.remove();
+    }
 }
 
 // check project status every interval (default to 2sec).
-setInterval(function() {
-    checkProjectStatus(projectId);
+setInterval(function () {
+    checkProjectStatus();
 }, interval);
